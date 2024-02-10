8eb36419980aa161b708ac265d6cbbcdcdda3f53
Ashton Dong <Ashton@MacBook-Pro-3.local>
2024-02-09 20:18:25 -0800

    update function now works

--- wrapper.rb
+++ wrapper.rb
@@ -14,6 +14,8 @@ end
 
 class Loader
 
+    INDEX = "short_bestsellers" # "new_bestsellers" is the full length data set
+
     def self.convert_csv
         file_path = "/Users/Ashton/Desktop/studysoup/elasticsearch/datafiles/bestsellers.csv"
 
@@ -36,42 +38,70 @@ class Loader
     end
 
 
-    def self.import file_path = "/Users/Ashton/Desktop/studysoup/elasticsearch/datafiles/bestsellers.csv"
+    def self.import
+
         client = Elasticsearch::Client.new
 
-        # csv and json file paths
-        csv_file = file_path
-        json_file = "/Users/Ashton/Desktop/studysoup/elasticsearch/datafiles/bestsellers.json"
+        # json file path
+        json_file = "/Users/Ashton/Desktop/studysoup/elasticsearch/datafiles/shortbestsellers.json"
 
         read_json = File.read(json_file)
 
         json_hashes = JSON.parse(read_json)
 
         json_hashes.each do |item|
-            client.index(index: "new_bestsellers", body: item)
+            client.index(index: INDEX, body: item)
         end
 
 
     end
 
-    def self.delete index, id=nil
+    def self.delete id=nil
         client = Elasticsearch::Client.new
 
         if id == nil
-            client.indices.delete(index: index)
+            client.indices.delete(index: INDEX)
         else 
-            client.delete(index: index, id: id)
+            client.delete(index: INDEX, id: id)
         end
         
     end
 
     # tested using params "new_bestsellers", "Non Fiction", "Lies"
-    def self.search index, genre, term
+    def self.search genre, term, year=nil
         client = Elasticsearch::Client.new
         # agg on genre and year, look into generating relevant top results
         # return query
-
-        client.search(index: index, body: { 
+        if year != nil
+            client.search(index: INDEX, body: { 
+            # aggs: {
+            #     "year": {
+            #         "filter": {
+            #             #date
+            #         }
+            #     }
+            # }
+            aggs: {
+                "genre_aggs":{
+                    "filter": {
+                        "term": {
+                            "Genre.keyword": genre
+                            }
+                        }
+                    
+                    }
+                },
+            query: {
+                "match":{
+                    "Name": {
+                        query: term
+                    } 
+                }
+            }
+        }
+      )
+        end
+        client.search(index: INDEX, body: { 
             # aggs: {
             #     "year": {
             #         "filter": {
@@ -100,9 +130,9 @@ class Loader
     )
     end
 
-    def self.update index, id, body
+    def self.update id, body
         client = Elasticsearch::Client.new
-        client.index(index: index, id: id, body: body)
+        client.update(index: INDEX, id: id, body: {doc: body})
     end
 
 end
